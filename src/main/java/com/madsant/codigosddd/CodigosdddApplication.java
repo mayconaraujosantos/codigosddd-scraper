a91359bbb3857030555c98daea3dc73c6ce518b5
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2022-12-01 22:35:54 -0300

    chore: add swagger configuration for api

---
+++ src/main/java/com/madsant/codigosddd/CodigosdddApplication.java
@@ -0,0 +1,13 @@
+package com.madsant.codigosddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigosdddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodigosdddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/madsant/codigosddd/CodigosdddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madsant.codigosddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigosdddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
